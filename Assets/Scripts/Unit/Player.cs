69868cbcc58ad130cdc8c2091ea469da1ba8c217
Johnny Park <pos02232@gmail.com>
2024-08-04 19:50:05 +0900

    캐릭터 충돌 및 VisibleEnum, 버튼 입력 추가

--- Assets/Scripts/Unit/Player.cs
+++ Assets/Scripts/Unit/Player.cs
@@ -1,21 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Unit
 {
+  [Serializable]
+  public enum ButtonInputType
+  {
+    Left,
+    Right,
+    Attack
+  }
+
   [RequireComponent(typeof(Rigidbody2D))]
   public class Player : MonoBehaviour
   {
     [Header("Movement")]
     public float moveSpeed = 5.0f;
     [ReadOnly] public Vector2 moveInput;
-    [ReadOnly] public Vector2 velocity;
 
     protected Rigidbody2D body;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
 
     private void Awake()
     {
       body = GetComponent<Rigidbody2D>();
       body.isKinematic = true;
+
+      contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+      contactFilter.useLayerMask = true;
+      contactFilter.useTriggers = false;
     }
 
     protected virtual void Update()
@@ -23,15 +39,46 @@ namespace Unit
       ComputeInput();
     }
 
-    protected virtual void FixedUpdate()
+    #region Input
+    private Dictionary<ButtonInputType, bool> buttonInputs = new Dictionary<ButtonInputType, bool>();
+
+    [VisibleEnum(typeof(ButtonInputType))]
+    public void OnButtonDown(int buttonInputType)
     {
-      velocity = moveInput * moveSpeed;
-      body.position += velocity * Time.deltaTime;
+      buttonInputs[(ButtonInputType)buttonInputType] = true;
+    }
+
+    [VisibleEnum(typeof(ButtonInputType))]
+    public void OnButtonUp(int buttonInputType)
+    {
+      buttonInputs[(ButtonInputType)buttonInputType] = false;
     }
 
     private void ComputeInput()
     {
-      moveInput = Input.GetAxisRaw("Horizontal") * Vector2.right;
+      moveInput = Vector2.zero;
+
+      // 터치 입력
+      if (buttonInputs.ContainsKey(ButtonInputType.Left) && buttonInputs[ButtonInputType.Left]) moveInput = Vector2.left;
+      else if (buttonInputs.ContainsKey(ButtonInputType.Right) && buttonInputs[ButtonInputType.Right]) moveInput = Vector2.right;
+      // 기타 입력
+      else moveInput = Input.GetAxisRaw("Horizontal") * Vector2.right;
+    }
+    #endregion
+
+    protected virtual void FixedUpdate()
+    {
+      Vector2 velocity = moveInput * moveSpeed;
+      float distance = velocity.magnitude * Time.deltaTime;
+
+      int count = body.Cast(velocity, contactFilter, hitBuffer, distance);
+      for (int i = 0; i < count; ++i)
+      {
+        distance = Math.Min(distance, hitBuffer[i].distance);
+      }
+
+      // Time.deltaTime이 epsilon이다
+      body.position += velocity.normalized * (distance - Time.deltaTime);
     }
   }
 }
