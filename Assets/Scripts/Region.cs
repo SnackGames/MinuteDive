f89c91e7405a2a7eb687f1d5312595342c4bc552
Johnny Park <pos02232@gmail.com>
2024-08-18 22:50:40 +0900

    region 기능 추가

---
+++ Assets/Scripts/Region.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class RegionEvent : UnityEvent<string> { }
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Region : MonoBehaviour
+{
+  public string regionName;
+
+  public RegionEvent OnRegionEnter;
+  public RegionEvent OnRegionExit;
+
+  protected Rigidbody2D body;
+  protected Collider2D regionCollider;
+
+  private void Awake()
+  {
+    body = GetComponent<Rigidbody2D>();
+    body.isKinematic = true;
+    regionCollider = GetComponent<Collider2D>();
+    regionCollider.isTrigger = true;
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    OnRegionEnter.Invoke(regionName);
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    OnRegionExit.Invoke(regionName);
+  }
+}
--- Assets/Scripts/Unit/Player.cs
+++ Assets/Scripts/Unit/Player.cs
@@ -210,7 +210,7 @@ namespace Unit
     public void AnimTrigger_Vibrate()
     {
       // #TODO 진동 세기, 시간 등 커스텀 되는 plugin 찾을것
-      UnityEngine.Handheld.Vibrate();
+      Handheld.Vibrate();
     }
 
     public void AnimTrigger_CameraShake(float strength = 1.0f) => playerCamera.ShakeCamera(strength);
