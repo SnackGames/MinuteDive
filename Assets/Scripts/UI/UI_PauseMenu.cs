5ce33dac3c8d77c68b47ddc777e190f8f06c676a
Johnny Park <pos02232@gmail.com>
2024-08-23 01:51:16 +0900

    일시 정지 추가, 폰트 추가

--- Assets/Scripts/UI/UI_PauseMenu.cs
+++ Assets/Scripts/UI/UI_PauseMenu.cs
@@ -6,5 +6,22 @@ namespace UI
   [AddComponentMenu("UI/Pause Menu")]
   public class UI_PauseMenu : MonoBehaviour
   {
+    public GameObject pauseUI;
+
+    public void PauseGame(bool pause)
+    {
+      Time.timeScale = pause ? 0.0f : 1.0f;
+      pauseUI.SetActive(pause);
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+      if (!hasFocus) PauseGame(true);
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+      if (pauseStatus) PauseGame(true);
+    }
   }
 }
