96485de8383a9e58b53c2ccd6fcd22686cb04adb
Johnny Park <pos02232@gmail.com>
2024-11-05 00:46:47 +0900

    층 무작위 선출 로직 추가

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -4,7 +4,7 @@ public class Floor : Region
 {
   public Vector2 GetFloorSize()
   {
-    Vector3 boundSize = regionCollider?.bounds.size ?? Vector3.zero;
+    Vector3 boundSize = GetComponent<Collider2D>()?.bounds.size ?? Vector3.zero;
     return new Vector2(boundSize.x, boundSize.y);
   }
 }
--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -1,4 +1,5 @@
 using Data;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FloorManager : MonoBehaviour
@@ -6,17 +7,41 @@ public class FloorManager : MonoBehaviour
   public Vector2 spawnStart;
   public DungeonData dungeonData;
 
-  private Floor[] floorList;
+  private List<Floor> floorList = new List<Floor>();
 
   public void GenerateFloors()
   {
     // 층 배열 초기화
-    if (floorList != null)
-      foreach (Floor floor in floorList)
-        if (floor != null)
-          Destroy(floor.gameObject);
+    foreach (Floor floor in floorList)
+      Destroy(floor.gameObject);
+    floorList.Clear();
 
     // 매 층마다 소환 가능한 층들을 선발해서 무작위로 생성
-    // sliding window로 추후에 더 최적화 가능.
+    // sliding window 기법으로 추후에 더 최적화 가능.
+    int floorNumber = 0;
+    Vector3 floorPosition = new Vector3(spawnStart.x, spawnStart.y, 0.0f);
+    List<FloorGenData> viableFloorList;
+    while (true)
+    {
+      floorNumber++;
+      viableFloorList = new List<FloorGenData>();
+
+      foreach (FloorGenData floorGenData in dungeonData.floorGenData)
+        if (floorNumber >= floorGenData.validFloorRange.x && floorNumber <= floorGenData.validFloorRange.y)
+          viableFloorList.Add(floorGenData);
+
+      if (viableFloorList.Count <= 0)
+        break;
+
+      FloorGenData pickedFloorGenData = viableFloorList[Random.Range(0, viableFloorList.Count - 1)];
+      float? pickedFloorHeight = pickedFloorGenData.floorPrefab.GetComponent<Floor>()?.GetFloorSize().y;
+
+      GameObject spawnedFloor = Instantiate(pickedFloorGenData.floorPrefab, floorPosition + new Vector3(0.0f, pickedFloorHeight * 0.5f ?? 0.0f, 0.0f), Quaternion.identity);
+      Floor floorComponent = spawnedFloor.GetComponent<Floor>();
+      if (floorComponent != null)
+        floorList.Add(floorComponent);
+
+      floorPosition.y += pickedFloorHeight ?? 0.0f;
+    }
   }
 }
