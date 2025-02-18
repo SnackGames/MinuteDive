683bbf4d6a4b9d49b27cce87a6e20b549a686f61
Johnny Park <pos02232@gmail.com>
2025-02-18 22:07:46 +0900

    Fragile Object 피격이 안되던 버그 수정

--- Assets/Scripts/Object/FragileObject.cs
+++ Assets/Scripts/Object/FragileObject.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class FragileObject : MonoBehaviour
 {
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    GetComponent<Health>()?.OnHit(1);
+  }
+
   public void OnHealthChanged(bool isHit, int prevHealth, int health)
   { 
     if (health <= 0)
