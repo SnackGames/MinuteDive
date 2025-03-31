99e86b4741e443c8096ebbcbc1070a3a550f6697
mmj4594 <csmmj4594@postech.ac.kr>
2025-03-31 20:28:03 +0900

    아이템 툴팁 텍스트 배치 수정, 게임오버 화면에서는 아이템 툴팁이 아이템의 좌/우가 아닌 위에 출력되도록 수정

--- Assets/Scripts/UI/Tooltips/TooltipManager.cs
+++ Assets/Scripts/UI/Tooltips/TooltipManager.cs
@@ -31,10 +31,26 @@ public class TooltipManager : MonoBehaviour
     currentTooltipOwner = tooltipOwner;
 
     // 툴팁 피벗 및 위치 오프셋 설정
-    bool isLeftSide = rectTransform.position.x < (Screen.width / 2f);
-    tooltipPanel.GetComponent<RectTransform>().pivot = isLeftSide ? new Vector2(0f, 0.5f) : new Vector2(1f, 0.5f);
-    float offsetX = (rectTransform.rect.width / 2f) + 40f;
-    Vector3 finalPos = rectTransform.position + new Vector3(isLeftSide ? offsetX : -offsetX, 0f, 0f);
+    Vector3 finalPos = rectTransform.position;
+    UI_InventoryItem inventoryItemUI = tooltipOwner.GetComponent<UI_InventoryItem>();
+    if (inventoryItemUI != null)
+    {
+      // 로비 인벤토리
+      if (inventoryItemUI.isEquippable)
+      {
+        bool isLeftSide = rectTransform.position.x < (Screen.width / 2f);
+        tooltipPanel.GetComponent<RectTransform>().pivot = isLeftSide ? new Vector2(0f, 0.5f) : new Vector2(1f, 0.5f);
+        float offsetX = (rectTransform.rect.width / 2f) + 40f;
+        finalPos = rectTransform.position + new Vector3(isLeftSide ? offsetX : -offsetX, 0f, 0f);
+      }
+      // 게임오버 화면
+      else
+      {
+        tooltipPanel.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0f);
+        float offsetY = (rectTransform.rect.height / 2f) + 40f;
+        finalPos = rectTransform.position + new Vector3(0f, offsetY, 0f);
+      }
+    }
 
     tooltipPanel.SetActive(true);
     tooltipPanel.GetComponent<UI_ItemTooltip>()?.setTooltipData(itemID);
@@ -45,6 +61,9 @@ public class TooltipManager : MonoBehaviour
     // 툴팁 소유자 지정
     currentTooltipOwner = null;
 
-    tooltipPanel?.SetActive(false);
+    if (tooltipPanel != null)
+    {
+      tooltipPanel.SetActive(false);
+    }
   }
 }
